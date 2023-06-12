6a3a35f7e6f77e45a554521d461880d2782f1907
Monfernape <engr.usmankhalil@gmail.com>
2023-06-12 06:01:35 +0500

    feat: added config for supabase

---
+++ services/supabase.ts
@@ -0,0 +1,28 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+import Constants from 'expo-constants';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = Constants.expoConfig!.extra!.SUPABASE_URL;
+const supabaseAnonKey = Constants.expoConfig!.extra!.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
